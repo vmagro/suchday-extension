9676a71c4ef4235579989148c46c9c439750f30c
Vinnie Magro <vmagro@socaldevs.com>
2014-04-13 09:02:12 -0700

    Open window when mode changes, need to make it just alter the current window now

--- background.js
+++ background.js
@@ -1,8 +1,28 @@
+//var baseUrl = 'http://suchday/';
+var baseUrl = 'http://dash.ptzlabs.com/';
+var userId = '102190104458073909670';
+var mode = 'blank';
+
 function poll(){
-  if(localStorage["userEmail"]){
-    console.log('polling');
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "http:/dash.ptzlabs.com/mode?user="+userId, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      var data = JSON.parse(xhr.responseText);
+      if(mode != data.mode){
+        mode = data.mode;
+        update();
+      }
+    }
   }
+  xhr.send();
+  setTimeout(function() { poll() }, 5000);
 }
 
-//every 5 seconds
-setInterval(poll, 5000);
+function update(){
+  chrome.windows.create({'url': 'http://ptzlabs.com/dw', 'type': 'popup'}, function(window) {
+    chrome.windows.update(window.id, {state: 'fullscreen'});
+  });
+}
+
+poll();
